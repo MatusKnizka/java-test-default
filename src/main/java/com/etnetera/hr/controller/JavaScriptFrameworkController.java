2dc35765116a911c853d5a1e8c0dfa4da0815bc1
Matus Knizka <knizkam@gmail.com>
2020-01-29 09:21:19 +0100

    CrossOrigin

--- src/main/java/com/etnetera/hr/controller/JavaScriptFrameworkController.java
+++ src/main/java/com/etnetera/hr/controller/JavaScriptFrameworkController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,8 @@ public class JavaScriptFrameworkController {
 		this.repository = repository;
 	}
 
+
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/frameworks")
 	public Iterable<JavaScriptFramework> frameworks(
 		@RequestParam(value="name", required=false) String name,
